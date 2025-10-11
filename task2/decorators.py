00267bba3a751fe82ead31baffdc915806621fac
Arslanov Suleiman <103919339+arslanov-sea@users.noreply.github.com>
2025-10-11 15:28:31 +0300

    Изменение декораторов

--- task2/decorators.py
+++ task2/decorators.py
@@ -2,15 +2,16 @@ from typing import List, Callable
 from student import Student
 
 
-class StudentRepDBDecorator:
-    def __init__(self, db_repo, filter_func: Callable[[Student], bool] = None,
+class StudentRepFilterSortDecorator:
+    def __init__(self, _repo, filter_func: Callable = None,
                  sort_key: Callable[[Student], any] = None):
-        self._db_repo = db_repo
+        self._repo = _repo
         self._filter_func = filter_func
         self._sort_key = sort_key
+        self._count = self._repo.get_count()
 
     def get_k_n_short_list(self, k: int, n: int) -> List[Student]:
-        students = self._db_repo.get_k_n_short_list(1000, 1)  # Получаем всех
+        students = self._repo.get_k_n_short_list(self._count, 1)  # Получаем всех
 
         if self._filter_func:
             students = list(filter(self._filter_func, students))
@@ -23,37 +24,7 @@ class StudentRepDBDecorator:
         return students[start_index:end_index] if start_index < len(students) else []
 
     def get_count(self) -> int:
-        students = self._db_repo.get_k_n_short_list(1000, 1)
-
-        if self._filter_func:
-            students = list(filter(self._filter_func, students))
-
-        return len(students)
-
-
-
-class StudentRepFileDecorator:
-    def __init__(self, file_repo, filter_func: Callable[[Student], bool] = None,
-                 sort_key: Callable[[Student], any] = None):
-        self._file_repo = file_repo
-        self._filter_func = filter_func
-        self._sort_key = sort_key
-
-    def get_k_n_short_list(self, k: int, n: int) -> List[Student]:
-        students = self._file_repo.read_all()
-
-        if self._filter_func:
-            students = list(filter(self._filter_func, students))
-
-        if self._sort_key:
-            students.sort(key=self._sort_key)
-
-        start_index = (n - 1) * k
-        end_index = start_index + k
-        return students[start_index:end_index] if start_index < len(students) else []
-
-    def get_count(self) -> int:
-        students = self._file_repo.read_all()
+        students = self._repo.get_k_n_short_list(self._count, 1)
 
         if self._filter_func:
             students = list(filter(self._filter_func, students))
